263ca61281fabf0fee51afccc3b71ab320cf4b70
Rowl1ng <ling.rowling.luo@gmail.com>
2023-07-20 09:52:22 +0100

    update

--- models/encoder/flow.py
+++
@@ -1,4 +0,0 @@
-from models.styleflow.flow import build_model
-
-def get_flow(style_dim=48, width=512, depth=4, condition_size=2):
-    return build_model(style_dim, (width,) * depth, condition_size, 1, True)
